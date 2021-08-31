473b1fca6c58402114dde1331043a85a948ba9c3
jdibra <86662406+jdibra@users.noreply.github.com>
2021-08-31 12:56:20 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'ibahimshaikh.com';
+    $email_subject = "new form submission";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to "gulamhussainshaikh136@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    headers("Location: index.html");
+
+?>
+
